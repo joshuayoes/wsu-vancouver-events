067bb11316e880b14aa365d9dee40583beda8b7b
joshuayoes <37849890+joshuayoes@users.noreply.github.com>
2019-09-01 14:24:37 -0700

    Updates

---
+++ precache-manifest.976d0be4e9598c14dd89034caf15e75c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f4253f11539a1322142e38fa0f2c57f",
+    "url": "/wsu-vancouver-events/index.html"
+  },
+  {
+    "revision": "c7c72977ff312e7d9d52",
+    "url": "/wsu-vancouver-events/static/css/2.c99ed502.chunk.css"
+  },
+  {
+    "revision": "abed2e5f0885660f0ef9",
+    "url": "/wsu-vancouver-events/static/css/main.4f2e2a9f.chunk.css"
+  },
+  {
+    "revision": "c7c72977ff312e7d9d52",
+    "url": "/wsu-vancouver-events/static/js/2.9819f9dd.chunk.js"
+  },
+  {
+    "revision": "abed2e5f0885660f0ef9",
+    "url": "/wsu-vancouver-events/static/js/main.8d042edc.chunk.js"
+  },
+  {
+    "revision": "b9878d9e8078a7264b9f",
+    "url": "/wsu-vancouver-events/static/js/runtime~main.e5293f6d.js"
+  }
+]);
